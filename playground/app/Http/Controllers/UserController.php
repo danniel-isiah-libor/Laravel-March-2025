746ec553fbcbd2aa5957967aa11b56f1867ee45b
Andrei <jackparquez1@gmail.com>
2025-03-28 18:59:30 +0800

    Add user management routes and work experiences functionality

---
+++ playground/app/Http/Controllers/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WorkExperience;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function workExperiences(Request $request)
+    {
+        $workExperiences = (new WorkExperience())->getRecords();
+
+        $companyName = $request->name ?? null;
+
+        if ($companyName) {
+            $workExperiences = array_filter(
+                $workExperiences,
+                function ($workExperience) use ($companyName) {
+                    return $workExperience['company_name'] == $companyName;
+                }
+            );
+        }
+
+        $html = "<ul>";
+
+        foreach ($workExperiences as $workExperience) {
+            $html .= "<li>";
+            $html .= "<h2>" . $workExperience['company_name'] . "</h2>";
+            $html .= "<p>Role: " . $workExperience['role'] . "</p>";
+            $html .= "<p>Tenure: " . $workExperience['tenure'] . "</p>";
+            $html .= "</li>";
+        }
+
+        $html .= "</ul>";
+
+        // return $html;
+        return view('work-experiences', [
+            'data' => $html
+            
+        ]);
+    }
+}
--- playground/routes/web.php
+++ playground/routes/web.php
@@ -1,7 +1,47 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return "Hello World";
 });
+
+
+
+Route::prefix('/admin')->group(function () {
+    Route::prefix('/user')->name('user.')->group(function () {
+        Route::get('/create', function () {
+            return "Create User";
+        })->name('create');
+        Route::get('/edit', function () {
+            return "Edit User";
+        })->name('edit');
+        Route::get('/delete', function () {
+            return "Delete User";
+        })->name('delete');
+    });
+});
+
+Route::fallback(function () {
+return "<h1>Page Not Found</h1>";
+});
+
+
+Route::get('/users/update/{id}
+
+', function ($id) {
+    //update user
+
+    return "Update User". $id;
+});
+
+Route::get('/request', function (Request $request) {
+    // dd($request->query('name', 'default'));
+   
+});
+
+Route::get('/work-experiences',[UserController::class, 'workExperiences'])
+->name('work-experiences');
