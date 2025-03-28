36dfedd4b7800e625b9a3f3a7b48ab7dd0a67acf
Danniel Libor <dlibor.dev@gmail.com>
2025-03-28 16:57:40 +0800

    [Day 1] routes

--- playground/routes/web.php
+++ playground/routes/web.php
@@ -1,7 +1,103 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+
+    // return "<h1 style='color: red'>
+    // Hello Laravel </h1>";
+    return "<script> alert('Hello Laravel') </script>";
+});
+
+Route::get('/admin/users/update/1', function () {
+    return "Admin Page";
+});
+
+Route::get('/admin/users/create', function () {
+    return "Admin Page";
+});
+
+Route::get('/admin/users/delete', function () {
+    return "Admin Page";
+});
+
+Route::get('/admin/dashboard', function () {
+    return "Admin Dashboard";
+});
+
+Route::prefix('/admin')->group(function () {
+    Route::prefix('/users')->name('users.')->group(function () {
+        Route::prefix('/config')->group(function () {
+            //
+        });
+
+        // Route::get('/update', function () {
+        //     //
+        // })->name('update');
+        Route::redirect('/update', '/create')
+            ->name('update');
+
+        Route::get('/create', function () {
+            //
+        })->name('create');
+
+        Route::get('/delete', function () {
+            //
+        })->name('delete');
+    });
+
+    Route::get('/dashboard', function () {
+        // $cars = (object)[
+        //     "brand" => "Honda",
+        //     "color" => "red"
+        // ];
+
+        // return $cars->color;
+
+        return redirect()->route('users.update');
+    });
+});
+
+Route::fallback(function () {
+    return "<h1> PAGE NOT FOUND </h1>";
+});
+
+Route::get('/users/update/{id}/{name?}', function ($id, $name = "John") {
+    // updating user record...
+    return "User Updated for ID: " . $id . ' ' . $name;
+})->where('id', '[0-9]+');
+
+// Route::get('/users/update/2', function () {
+//     // updating user record...
+//     return "User Updated";
+// });
+
+// Route::get('/users/update/3', function () {
+//     // updating user record...
+//     return "User Updated";
+// });
+
+
+// Route::get('/profile', function () {
+//     //
+// })->name('show');
+
+// Route::post('/profile', function () {
+//     //
+// })->name('update');
+
+// Route::match(['get', 'post'], '/profile', function (Request $request) {
+//     if ($request->isMethod('get')) {
+//         // get
+//     } else {
+//         // post
+//     }
+// });
+
+Route::get('/request', function (Request $request) {
+    // dd($request->query('name', 'John'));
+    // dd($request->all());
+    dd($request->name);
 });
